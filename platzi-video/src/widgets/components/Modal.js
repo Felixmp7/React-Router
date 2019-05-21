5278c9148a05bf7be2c36dd2c693ff26c84799a6
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-21 16:58:42 -0400

    Finalizando clase de parametros de navegacion

--- platzi-video/src/widgets/components/Modal.js
+++ platzi-video/src/widgets/components/Modal.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import './Modal.css'
+import {Link} from 'react-router-dom'
 
 const Modal = props => {
   return(
     <div className="Modal">
       {props.children}
-      <button
-        className="Modal-close"
-        onClick={props.handleClick}
-      />
+      <Link
+        to={{
+          pathname: '/videos',
+          state: {
+            modal: false
+          }
+        }}
+        >
+        <button
+          className="Modal-close"
+          onClick={props.handleClick}
+        />
+      </Link>
     </div>
   )
 }
