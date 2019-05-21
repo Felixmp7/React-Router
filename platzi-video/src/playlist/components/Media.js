cbefa8c11ff35ae65d502ec9773d9debc649f352
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-21 16:37:38 -0400

    Envolviendo Media en un Link

--- platzi-video/src/playlist/components/Media.js
+++ platzi-video/src/playlist/components/Media.js
@@ -1,6 +1,7 @@
 //Dependencies
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 //Medias
 import './media.css'
 
@@ -11,19 +12,21 @@ class Media extends PureComponent {
   }
   render(){
     return(
-      <div className="Media" onClick={this.handleClick}>
-        <div className="Media-cover">
-          <img
-            className="Media-image"
-            src={this.props.cover}
-            alt="bitcoin"
-            width={260}
-            height={160}
-          />
+      <Link>
+        <div className="Media" onClick={this.handleClick}>
+          <div className="Media-cover">
+            <img
+              className="Media-image"
+              src={this.props.cover}
+              alt="bitcoin"
+              width={260}
+              height={160}
+            />
+          </div>
+          <h3 className="Media-title">{this.props.title}</h3>
+          <p className="Media-author">{this.props.author}</p>
         </div>
-        <h3 className="Media-title">{this.props.title}</h3>
-        <p className="Media-author">{this.props.author}</p>
-      </div>
+      </Link>
     )
   }
 }
