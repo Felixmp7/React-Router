49ce3828a3b571069fdd24d0356e82c9dcea33b0
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-22 15:15:52 -0400

    Practicando prueba de primer servidor con express

--- platzi-video/src/server.js
+++ platzi-video/src/server.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('*', (request,response) => {
+  console.log(request.url);
+  response.write('Hola Mundo' , request.url)
+  response.end()
+})
+
 app.listen(3000)
